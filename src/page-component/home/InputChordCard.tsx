b8536db85c5743a9b6183b46339d4dd1c405d9c3
Yudai Kawakami <yudainepnep@icloud.com>
2022-08-31 00:46:02 +0900

    :adhesive_bandage: fix

--- src/page-component/home/InputChordCard.tsx
+++ src/page-component/home/InputChordCard.tsx
@@ -20,7 +20,7 @@ const InputChordCard = () => {
           <input
             type="text"
             placeholder="CM7"
-            className={`text-center w-64 h-full p-6 rounded border shadow   ${
+            className={`text-center w-64 h-full p-4 rounded border shadow   ${
               largerThanXs ? "text-7xl" : "text-5xl"
             }  ${
               dark
@@ -29,7 +29,7 @@ const InputChordCard = () => {
             }`}
           />
         </div>
-        <span className="flex justify-end mt-1">
+        <span className="flex justify-end mt-3">
           <Button
             size={`${largerThanXs ? "sm" : "xs"}`}
             color="dark"
@@ -37,7 +37,7 @@ const InputChordCard = () => {
             className={`${dark ? "bg-purple-600" : undefined}`}
           >
             <IconPlayerPlay size={14} />
-            <span className=" ml-1">Play</span>
+            <span className="ml-1">Play</span>
           </Button>
         </span>
       </Paper>
