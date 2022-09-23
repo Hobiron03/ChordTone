84358bf03f8558df635b3fb57a375a8cb9ea5ef6
Yudai Kawakami <yudainepnep@icloud.com>
2022-09-23 17:46:20 +0900

    :sparkles: 鍵盤に度数を表示

--- src/page-component/index/Piano/WhiteKey.tsx
+++ src/page-component/index/Piano/WhiteKey.tsx
@@ -1,8 +1,11 @@
 import { Text, useMantineColorScheme } from "@mantine/core";
-import { FC, useState } from "react";
-import { useMediaQuery } from "src/lib/mantine";
+import { FC, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
+import { useMediaQuery } from "src/lib/mantine";
+import { selectChord } from "src/state/selectChordSlice";
 import { useChordTones } from "src/hooks/useChordTones";
+import { Chord } from "@tonaljs/tonal";
 
 type Props = {
   notes: string[];
@@ -14,15 +17,32 @@ export const WhiteKey: FC<Props> = (props) => {
   const { colorScheme } = useMantineColorScheme();
   const dark = colorScheme === "dark";
   const largerThanSm = useMediaQuery("sm");
+  const chord = useSelector(selectChord);
 
-  const { isChordTone } = useChordTones(props.index);
+  const { isChordTone, setIsChordTone } = useChordTones(props.index);
   const [intreval, setInterval] = useState<string>("");
+
+  useEffect(() => {
+    if (isChordTone) {
+      Chord.get(chord).notes.forEach((note, index) => {
+        if (props.notes.includes(note)) {
+          setInterval(Chord.get(chord).intervals[index]);
+        }
+      });
+    } else {
+      setInterval("");
+    }
+  }, [isChordTone]);
+
+  useEffect(() => {
+    setIsChordTone(false);
+  }, [chord]);
 
   return (
     <div
       className={`flex justify-center items-center ${
         isChordTone
-          ? "bg-red-200 border-gray-300"
+          ? "bg-yellow-200 border-gray-300"
           : dark
           ? "bg-gray-300 border-gray-400"
           : "bg-white border-gray-300"
@@ -30,7 +50,7 @@ export const WhiteKey: FC<Props> = (props) => {
         largerThanSm ? "w-14 h-44" : "w-12 h-40"
       }  border border-solid cursor-pointer`}
     >
-      <Text className="pt-28 text-sm">{intreval}</Text>
+      <Text className="pt-28 text-sm text-m_dark-9 font-bold">{intreval}</Text>
     </div>
   );
 };
--- src/page-component/index/Piano/page.tsx
+++ src/page-component/index/Piano/page.tsx
@@ -15,37 +15,76 @@ export const Piano: FC = () => {
 
   useEffect(() => {
     setCurrentChord(chord);
-    console.log(chord);
   }, []);
 
   return (
     <ScrollArea className="max-w-5xl" type="always">
       <div className="flex justify-center mt-5 ">
         <div className="flex relative">
-          <WhiteKey notes={["C"]} index={0} />
-          <BlackKey left={largerThanSm ? 35 : 30} index={1} />
-          <WhiteKey notes={["D"]} index={2} />
-          <BlackKey left={largerThanSm ? 92 : 80} index={3} />
-          <WhiteKey notes={["E"]} index={4} />
-          <WhiteKey notes={["F"]} index={5} />
-          <BlackKey left={largerThanSm ? 205 : 175} index={6} />
-          <WhiteKey notes={["G"]} index={7} />
-          <BlackKey left={largerThanSm ? 260 : 224} index={8} />
-          <WhiteKey notes={["A"]} index={9} />
-          <BlackKey left={largerThanSm ? 315 : 270} index={10} />
-          <WhiteKey notes={["B"]} index={11} />
-          <WhiteKey notes={["C"]} index={12} />
-          <BlackKey left={largerThanSm ? 428 : 367} index={13} />
-          <WhiteKey notes={["D"]} index={14} />
-          <BlackKey left={largerThanSm ? 485 : 416} index={15} />
-          <WhiteKey notes={["E"]} index={16} />
-          <WhiteKey notes={["F"]} index={17} />
-          <BlackKey left={largerThanSm ? 595 : 512} index={18} />
-          <WhiteKey notes={["G"]} index={19} />
-          <BlackKey left={largerThanSm ? 652 : 560} index={20} />
-          <WhiteKey notes={["A"]} index={21} />
-          <BlackKey left={largerThanSm ? 706 : 607} index={22} />
-          <WhiteKey notes={["B"]} index={23} />
+          <WhiteKey notes={["C", "B#"]} index={0} />
+          <BlackKey
+            notes={["C#", "Db"]}
+            left={largerThanSm ? 35 : 30}
+            index={1}
+          />
+          <WhiteKey notes={["D", "C##"]} index={2} />
+          <BlackKey
+            notes={["D#", "Eb"]}
+            left={largerThanSm ? 92 : 80}
+            index={3}
+          />
+          <WhiteKey notes={["E", "D##"]} index={4} />
+          <WhiteKey notes={["F", "E#"]} index={5} />
+          <BlackKey
+            notes={["F#", "Gb"]}
+            left={largerThanSm ? 205 : 175}
+            index={6}
+          />
+          <WhiteKey notes={["G", "F##"]} index={7} />
+          <BlackKey
+            notes={["G#", "Ab"]}
+            left={largerThanSm ? 260 : 224}
+            index={8}
+          />
+          <WhiteKey notes={["A", "G##"]} index={9} />
+          <BlackKey
+            notes={["A#", "Bb"]}
+            left={largerThanSm ? 315 : 270}
+            index={10}
+          />
+          <WhiteKey notes={["B", "A##"]} index={11} />
+          <WhiteKey notes={["C", "B#"]} index={12} />
+          <BlackKey
+            notes={["C#", "Db"]}
+            left={largerThanSm ? 428 : 367}
+            index={13}
+          />
+          <WhiteKey notes={["D", "C##"]} index={14} />
+          <BlackKey
+            notes={["D#", "Eb"]}
+            left={largerThanSm ? 485 : 416}
+            index={15}
+          />
+          <WhiteKey notes={["E", "D##"]} index={16} />
+          <WhiteKey notes={["F", "E#"]} index={17} />
+          <BlackKey
+            notes={["F#", "Gb"]}
+            left={largerThanSm ? 595 : 512}
+            index={18}
+          />
+          <WhiteKey notes={["G", "F##"]} index={19} />
+          <BlackKey
+            notes={["G#", "Ab"]}
+            left={largerThanSm ? 652 : 560}
+            index={20}
+          />
+          <WhiteKey notes={["A", "G##"]} index={21} />
+          <BlackKey
+            notes={["A#", "Bb"]}
+            left={largerThanSm ? 706 : 607}
+            index={22}
+          />
+          <WhiteKey notes={["B", "A##"]} index={23} />
         </div>
       </div>
     </ScrollArea>
