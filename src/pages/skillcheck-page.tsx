7c619319bc9b766fca53e1e410563006e66729e7
Yudai Kawakami <yudainepnep@icloud.com>
2022-12-25 23:29:10 +0900

    :sparkles: skill check page の土台を作成

--- src/pages/skillcheck-page.tsx
+++ src/pages/skillcheck-page.tsx
@@ -1,7 +1,13 @@
 import { NextPage } from "next";
+import { Layout } from "src/component/Layout";
+import { SkillCheck } from "src/page-component/skill-check/index";
 
 const SkillCheckPage: NextPage = () => {
-  return <div>SkillCheckPage</div>;
+  return (
+    <Layout title="Skill Check">
+      <SkillCheck />
+    </Layout>
+  );
 };
 
 export default SkillCheckPage;
