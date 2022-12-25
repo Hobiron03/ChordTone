9ad012aa3f11ffd9456b172872beaac8ed204ebc
Yudai Kawakami <yudainepnep@icloud.com>
2022-12-25 23:44:14 +0900

    :sparkles: create comming soon page

---
+++ src/page-component/comming-soon/skill-check/page.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@mantine/core";
+import { FC } from "react";
+
+/** @package */
+export const CommingSoon: FC = () => {
+  return (
+    <Container size="md">
+      <div>
+        <p>Comming Soon...</p>
+      </div>
+    </Container>
+  );
+};
--- src/pages/skillcheck-page.tsx
+++ src/pages/skillcheck-page.tsx
@@ -1,11 +1,13 @@
 import { NextPage } from "next";
 import { Layout } from "src/component/Layout";
+import { CommingSoon } from "src/page-component/comming-soon/skill-check";
 import { SkillCheck } from "src/page-component/skill-check/index";
 
 const SkillCheckPage: NextPage = () => {
   return (
     <Layout title="Skill Check">
-      <SkillCheck />
+      {/* <SkillCheck /> */}
+      <CommingSoon />
     </Layout>
   );
 };
