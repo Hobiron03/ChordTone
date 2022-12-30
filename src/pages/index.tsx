4c2e8160486eaf704811a9c2ef33a356aeb85458
Yudai Kawakami <yudainepnep@icloud.com>
2022-12-30 17:24:40 +0900

    :adhesive_bandage: タブタイトルの文言を修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Index } from "src/page-component/index/index";
 
 const IndexPage: NextPage = () => {
   return (
-    <Layout title="Chord Tone - コードの和音（トーン）を調べられるサービス">
+    <Layout title="Chord Tone - 和音の構成音（トーン）を調べられるサービス">
       <Index />
     </Layout>
   );
